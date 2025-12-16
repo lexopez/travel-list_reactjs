46f619ce95d15a5f01ab4a39294c499d7d67ee10
alexander lopez <iamlexlopez@gmail.com>
2025-12-16 18:59:40 +0800

    update app and form component
    add useState in the app component, storing items, and add callback function to set items from the form component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Form from "./Form";
 import Logo from "./Logo";
 import PackingList from "./PackingList";
 import Stats from "./Stats";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  console.log(items);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
+      <Form onAddItems={handleAddItems} />
       <PackingList />
       <Stats />
     </div>
