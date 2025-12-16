268318c07cbef60c49638dabf705d8bd9893f8f6
alexander lopez <iamlexlopez@gmail.com>
2025-12-16 19:43:07 +0800

    pass items to PackingLIst and to Item component

--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -1,18 +1,22 @@
-function PackingList() {
+import Item from "./Item";
+
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        <li>
-          <input type="checkbox" value={true} />
-          <span style={{ textDecoration: "line-through" }}>1 bag</span>
-          <button>❌</button>
-        </li>
-        <li>
-          <input type="checkbox" />
-          <span>1 t-shirt</span>
-          <button>❌</button>
-        </li>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
       </ul>
+
+      <div className="actions">
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed</option>
+        </select>
+        <button>Clear list</button>
+      </div>
     </div>
   );
 }
